ed0022123f65ec253cce6b992cb1d25a8c345c69
Kim Korsching <k.korsching@reply.de>
2022-01-24 14:58:01 +0100

    add tabs to plugin ui

--- src/ui.vue
+++ src/ui.vue
@@ -1,52 +1,69 @@
 <template>
   <div>
-    <!-- TEST Buttons -->
-    <button
-      class="button button--primary"
-      @click="createNode"
-    >
-    Create Page
-    </button>
-    <button
-      class="button button--primary"
-      @click="createPagesFromTemplate"
-    >
-    Create Pages From Template
-    </button>
-    <button
-      class="button button--primary"
-      @click="createPageAtIndex"
-    >
-    Create Page At Index
-    </button>
-    <button
-      class="button button--primary"
-      @click="removeAllPages"
-    >
-    Remove all Pages
-    </button>
-    <button
-      class="button button--primary"
-      @click="listPages"
-    >
-    List Pages
-    </button>
-    <p class="type type--pos-small-normal">
-      {{message}}
-    </p>
-    <!-- DEBUG Output -->
-      pageNames:
-      <ul v-if="pageNames">
-        <li v-for="name in pageNames" :key="name">
-          <pre>{{name}}</pre>
-        </li>
-      </ul>
-      Template:
-      <ul v-if="template">
-        <li v-for="page in template" :key="page.name">
-          <pre>{{page.name}} {{doesPageExistByName(page.name)}}</pre>
-        </li>
-      </ul>
+    <button class="button button--primary" @click="selectedTab = 1">Pages</button>
+    <button class="button button--primary" @click="selectedTab = 2">Settings</button>
+    <button class="button button--primary" @click="selectedTab = 3">Debug</button>
+    <!-- PAGES TAB -->
+    <div v-show="selectedTab === 1">
+      <div v-if="template">
+        <ul>
+          <li v-for="page in template" :key="page.name">
+            <pre>{{page.name}} : {{page.exists}}</pre>
+          </li>
+        </ul>
+      </div>
+      <div v-else>There is no template.</div>
+    </div>
+
+    <!-- SETTINGS TAB -->
+    <div v-show="selectedTab === 2"></div>
+
+    <!-- DEBUG TAB -->
+    <div v-show="selectedTab === 3">
+      <!-- TEST Buttons -->
+      <button
+        class="button button--primary"
+        @click="createNode"
+      >
+      Create Page
+      </button>
+      <button
+        class="button button--primary"
+        @click="createPagesFromTemplate"
+      >
+      Create Pages From Template
+      </button>
+      <button
+        class="button button--primary"
+        @click="createPageAtIndex"
+      >
+      Create Page At Index
+      </button>
+      <button
+        class="button button--primary"
+        @click="removeAllPages"
+      >
+      Remove all Pages
+      </button>
+      <button
+        class="button button--primary"
+        @click="listPages"
+      >
+      List Pages
+      </button>
+      <p class="type type--pos-small-normal">
+        {{message}}
+      </p>
+      <!-- DEBUG Output -->
+        pageNames:
+        <ul v-if="pageNames">
+          <li v-for="name in pageNames" :key="name">
+            <pre>{{name}}</pre>
+          </li>
+        </ul>
+        template:
+        <pre>{{template}}</pre>
+    </div>
   </div>
 </template>
 
@@ -63,6 +80,7 @@ export default {
     return {
       message: "",
       pageNames: [],
+      selectedTab: 1,
       // Example result from loading a CSV file
       template: [
         { type: "P", name: "🔺 Cover" },
